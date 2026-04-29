343e8b31b1108c881432aedabde72eed58808f65
Harrison Li <hli76@student.ubc.ca>
2026-04-29 13:57:47 -0700

    add projection to preprocessing

--- bird_dashboard/global.R
+++ bird_dashboard/global.R
@@ -4,7 +4,17 @@ library(terra)
 library(dplyr)
 library(stringr)
 library(bslib)
+library(bsicons)
 library(RColorBrewer)
+library(shinycssloaders)
+library(shinydashboard)
+library(readxl)
+
+addResourcePath("img", "img")
+
+# Load species metadata
+species_info_df <- read_excel("sample_data/V5ModelSummary_CAWA.xlsx", sheet = "species") %>%
+  select(id, scientific, english, french, family)
 
 # ---- Directories ----
 input_dir  <- "sample_data/model_predictions"
@@ -24,17 +34,23 @@ for (f in tif_files) {
   
   out_file <- file.path(output_dir, basename(f))
   
+  # Inside your for loop in global.R:
   if (!file.exists(out_file)) {
-    
-    message("Converting to COG: ", basename(f))
+    message("Converting and Projecting to COG: ", basename(f))
     
     r <- rast(f)
+    
+    # Reproject to WGS84 (EPSG:4326)
+    if (!terra::is.lonlat(r)) {
+      r <- terra::project(r, "EPSG:3857")
+    }
     
     writeRaster(
       r,
       out_file,
       filetype = "COG",
-      overwrite = TRUE
+      overwrite = TRUE,
+      gdal = c("COMPRESS=DEFLATE", "TILING=YES") # Standard COG optimizations
     )
   } else {
     message("File ", basename(f), " exists already")
@@ -65,16 +81,13 @@ raster_meta <- data.frame(
 get_raster <- function(key) {
 
   f <- file.path(output_dir, paste0(key, ".tif"))
+  
+  if(!file.exists(f)) return(NULL)
 
   r <- rast(f)
 
   # first band only
   if (nlyr(r) > 1) r <- r[[1]]
-
-  # ensure CRS once
-  if (!terra::is.lonlat(r)) {
-    r <- terra::project(r, "EPSG:4326")
-  }
   
   r <- terra::aggregate(r, fact = 2)
   
